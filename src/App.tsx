455a46d546ee53d1bfbd6d7f2ade1e505362c7d5
Abián Izquierdo <abizmo@gmail.com>
2022-06-13 17:14:10 +0100

    build: install styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
+import styled from 'styled-components';
+
 import './App.css';
 
+const Wrapper = styled.div`
+  padding: 4rem;
+  background: papayawhip;
+`;
+
 function App() {
-  return <h1>Hola!</h1>;
+  return (
+    <Wrapper>
+      <h1>Hola!</h1>
+    </Wrapper>
+  );
 }
 
 export default App;
